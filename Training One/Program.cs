b98cec8739549a6ce5341d6f69a79d8a9b23c6a3
Ram1476 <ramilangovan.1299@gmail.com>
2023-01-31 12:43:40 +0530

    new

--- Training One/Program.cs
+++ Training One/Program.cs
@@ -13,6 +13,7 @@ namespace Training_One
             Console.WriteLine("Hello World - Demo");
             Console.WriteLine("Hello World - 26666");
             Console.WriteLine("This is a newly Added Line- demo");
+            Console.WriteLine("New Line Added");
             Console.ReadLine();
         }
     }
